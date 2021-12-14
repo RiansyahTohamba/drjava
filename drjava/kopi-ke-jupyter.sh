3f37ab021cd319c58c617c99ba69f8322e0ca339
riansyah <mriansyah93@gmail.com>
2021-12-15 06:04:59 +0800

    ad sh

---
+++ drjava/kopi-ke-jupyter.sh
@@ -0,0 +1,3 @@
+# create view untuk sqlite 
+cp 2-tahap-1-drjava.sqlite ../../../studi_mandiri/refactoring_data/dch-jcodeodor/drjava/sqlite-jcodeodor/database/
+sqlitebrowser ../../../studi_mandiri/refactoring_data/dch-jcodeodor/drjava/sqlite-jcodeodor/database/2-tahap-1-drjava.sqlite
